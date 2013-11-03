c8c65e9f3a65178c3cd07951915d86f993b4217f
Aaron Krebs <akrebs@ualberta.ca>
2013-11-03 01:14:37 -0700

    Actions module test method stubs.

--- test/unit/testActions.c
+++ test/unit/testActions.c
@@ -10,7 +10,44 @@
 #include <unistd.h>
 
 #include "testActions.h"
+#include "Actions.h"
 
-char * test_all_Actions() {
+char *test_assertOpCompat() {
+	// TODO
+	return NULL;
+}
+
+char *test_assignmentCompatEh() {
+	// TODO
+	return NULL;
+}
+
+char *test_enterConstDeclPart() {
+	// function does nothing, so nothing to test
+	return NULL;
+}
+
+char *test_doConstDecl(){
+	// TODO
+	return NULL;
+}
+
+char *test_enterTypeDecltPart() {
+	// function does nothing, so nothing to test
+	return NULL;
+}
+
+char *test_doTypeDecl() {
+	// TODO
+	return NULL;
+}
+
+char *test_all_Actions() {
+	mu_run_test(test_assertOpCompat);
+	mu_run_test(test_assignmentCompatEh);
+	mu_run_test(test_enterConstDeclPart);
+	mu_run_test(test_doConstDecl);
+	mu_run_test(test_enterTypeDecltPart);
+	mu_run_test(test_doTypeDecl);
 	return NULL;
 }
