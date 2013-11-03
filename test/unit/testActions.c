8ac67e5992f80bbefa470b683b5e82e6336db776
Aaron Krebs <akrebs@ualberta.ca>
2013-11-02 23:35:08 -0600

    Actions module test skeletons. Also fixed some minor compile things in Hash test suite.

---
+++ test/unit/testActions.c
@@ -0,0 +1,16 @@
+/*
+ * Cmput 415 - Team YACC
+ * Unit tests for the Actions module
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+#include <unistd.h>
+
+#include "testActions.h"
+
+char * test_all_Actions() {
+	return NULL;
+}
---
+++ test/unit/testActions.h
@@ -0,0 +1,3 @@
+#include "minunit.h"
+
+char *test_all_Actions();
