047c48af3299df9e3ce88aea0f3285749b83305f
Sarah Anderson <sma2@cs.ualberta.ca>
2013-11-02 20:28:14 -0600

    Added function comments.

--- src/Hash.c
+++ src/Hash.c
@@ -6,32 +6,95 @@
 #include "Hash.h"
 
 
+/*
+ *
+ * Parameters:
+ *
+ * Return:
+ *
+ */
+
+
 //Simple hash function for now. Will replace later
 unsigned int getHashedKey(char *string) {
         return (string[0] % TABLE_SIZE);   
 }
 
-// int isSameKey(char *key) {
-//     int index = simpleHashFunc(key);
-    
-//     if ( hash[index] != NULL ) {
-//         if ( strcmp(hash[index]->key, key) == 0 ) {        
-//             return 1;
-//         }
-//     }   
-    
-//     return 0;
-// }
 
 
+/* Determines if key is in the hash bucket.
+ *
+ * Parameters: 
+ *              key: hash key
+ *
+ * Return: Boolean: 1 on in bucket and 0 for not in bucket.
+ */
+int isKeyInBucket(char *key) {
+        if ( findHashElementByKey(key) != NULL ){
+                return 1;
+        }
+
+        return 0;
+}
 
 
+/* Determines if provided hash element has same key as parameter.
+ *
+ * Parameters: 
+ *              element: hash element to compare against
+ *              key: hash key to compare against
+ *
+ * Return: Boolean: 1 on keys same and 0 for differnt keys.
+ */
+int isKeysIdentical(struct hashElement *element, char *key) {
+        if ( element != NULL ) {
+                if ( strcmp(element->key, key) == 0 ) {        
+                        return 1;
+                }
+        }   
+            
+        return 0;
+}
 
+
+/* Determines if provided in will cause hash collision
+ *
+ * Parameters: 
+ *              key: hash key
+ *
+ * Return: Boolean: 1 on collision and 0 for no collision.
+ */
+int isKeyCollison(char *key) {
+        if ( symbolTable[getHashedKey(key)] != NULL) {
+                return 1;
+        }
+
+        return 0;
+}
+
+
+/* Frees memory to members of hashElement struct
+ *
+ * Parameters: 
+ *              element: struct to be freed
+ *
+ * Return: void
+ *
+ * TODO: Add free calls for any symbol additions
+ */
 void freeHashElement(struct hashElement *element) {
         free(element->key);
         free(element);
 }
 
+
+/* Recursive function to free memory in a linked list.
+ *
+ * Parameters: 
+ *              current: current memember in linked list recursing through
+ *
+ * Return: void
+ */
 void deleteHashBucket(struct hashElement *current) {
         if ( current->next != NULL ) {
                 deleteHashBucket(current->next);
@@ -40,6 +103,13 @@ void deleteHashBucket(struct hashElement *current) {
         freeHashElement(current);
 }
 
+
+/* Destroys the symbol table for program end. Frees and alloced memory.
+ *
+ * Parameters: void
+ *
+ * Return: void
+ */
 void destroySymbolTable() {
         for (int i = 0; i < TABLE_SIZE; ++i) {
                 if ( symbolTable[i] != NULL ) {     
@@ -49,14 +119,14 @@ void destroySymbolTable() {
         }
 }
 
-int isKeyInBucket(char *key) {
-        if ( findHashElementByKey(key) != NULL ){
-                return 1;
-        }
 
-        return 0;
-}
-
+/* Gets pointer to hash element requested
+ *
+ * Parameters: 
+ *              key: hash key
+ *
+ * Return: Pointer to struct hashElement
+ */
 struct hashElement *findHashElementByKey(char *key) {
         struct hashElement *element = symbolTable[getHashedKey(key)];
 
@@ -81,9 +151,16 @@ struct hashElement *findHashElementByKey(char *key) {
         return NULL;
 }
 
-/*
- Prints the symbol table to stdout.
-*/
+
+/* Prints the symbol table to stdout. 
+ *
+ * Parameters: void
+ *
+ * Return: void
+ *
+ * TODO: Add values for the symbol info 
+ *              and remove value attribute.
+ */
 void dumpSymbolTable() {
         struct hashElement *element;
 
@@ -108,6 +185,19 @@ void dumpSymbolTable() {
         printf("DUMP COMPLETE.\n\n");
 }
 
+
+/* Creates and allocs memory for a struct hashElement.
+ *
+ * Parameters: 
+ *              key: hash key
+ *              value: int value for testing (WILL BE REMOVED!
+ *                      and replaced with real symbol info)
+ *
+ * Return: pointer to a newly create struct hashElement.
+ *
+ * TODO: add real parameters for when adding symbols to table   
+ *              - add symbol null pointer
+ */
 struct hashElement *createNewElement(char *key, int value) {
         struct hashElement *element = malloc(sizeof(struct hashElement));
 
@@ -127,6 +217,17 @@ struct hashElement *createNewElement(char *key, int value) {
         return element;
 }
 
+
+/* Appends the hashElement pointed to by newElement to the 
+ * end of the linked list pointed to by bucketHead
+ *
+ * Parameters:
+ *              bucketHead: head of the hash bucket linked list
+ *              newElement: new elment to append
+ *
+ * Return: void
+ *
+ */
 void appendToHashBucket(struct hashElement *bucketHead, struct hashElement *newElement) {
     struct hashElement *current = bucketHead;
 
@@ -136,26 +237,18 @@ void appendToHashBucket(struct hashElement *bucketHead, struct hashElement *newE
     newElement->prev = current;
 }
 
-int isKeysIdentical(struct hashElement *element, char *key) {
-        if ( element != NULL ) {
-                if ( strcmp(element->key, key) == 0 ) {        
-                        return 1;
-                }
-        }   
-            
-        return 0;
-}
 
-int isKeyCollison(char *key) {
-
-        if ( symbolTable[getHashedKey(key)] != NULL) {
-                return 1;
-        }
-
-        return 0;
-}
-
-
+/* Creates an entry in the symbol table for the key supplied.
+ *      
+ * Parameters: 
+ *              key: hash key
+ *              value: int value for testing (WILL BE REMOVED!
+ *                      and replaced with real symbol info)
+ *
+ * Returns: Boolean: 1 on success and 0 and failure.
+ * 
+ * TODO: add real parameters for when adding symbols to table      
+*/
 int createHashElement(char *key, int value) {
         struct hashElement *element;
         int index = getHashedKey(key);
