5b6f2dc184718f8230d3698be342489c509e665c
Guanqi Huang <guanqi@cs.ualberta.ca>
2013-11-06 15:18:52 -0700

    Added srcs for ElmementArray module

---
+++ test/unit/testElementArray.h
@@ -0,0 +1,14 @@
+#ifndef TESTELEMENTARRAY_H
+#define TESTELEMENTARRAY_H
+
+#include "minunit.h"
+#include "../../src/ElementArray.h"
+#include "testSymbol.h"
+
+char *test_newElementArray();
+char *test_growElementArray();
+char *test_appendElement();
+char *test_getElementAt();
+char *test_freeElementArray();
+char *test_all_ElementArray();
+#endif
