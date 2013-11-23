caf041082163d0b5379563d23f650bf2639f67a6
Aaron Krebs <akrebs@ualberta.ca>
2013-11-22 21:23:31 -0700

    Whitespace cleanup.

--- test/unit/testSymbol.h
+++ test/unit/testSymbol.h
@@ -1,22 +1,22 @@
 #ifndef TESTSYMBOL_H
 #define TESTSYMBOL_H
 #include "minunit.h"
-#include "../../src/Symbol.h"
+#include "Symbol.h"
 
-Symbol * setUpTypeSymbol();
-Symbol * setUpConstSymbol();
-Symbol * setUpIntConst();
+Symbol *setUpTypeSymbol();
+Symbol *setUpConstSymbol();
+Symbol *setUpIntConst();
 
-char * test_newAnonArraySym();
-char * test_newConstProxySym();
-char * test_newConstSymFromProxy();
-char * test_newParamSym();
-char * test_newTypeSymFromSym();
-char * test_newVariableSym();
-char * test_newSubrangeSym();
-char * test_newParameter;
-char * test_all_Symbol();
-char * test_isValidArrayAccess();
-char * test_newAnonScalarSym();
-char * test_isConstInScalar();
+char *test_newAnonArraySym();
+char *test_newConstProxySym();
+char *test_newConstSymFromProxy();
+char *test_newParamSym();
+char *test_newTypeSymFromSym();
+char *test_newVariableSym();
+char *test_newSubrangeSym();
+char *test_newParameter;
+char *test_all_Symbol();
+char *test_isValidArrayAccess();
+char *test_newAnonScalarSym();
+char *test_isConstInScalar();
 #endif
