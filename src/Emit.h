aa122b0d53e0b37f0f872577d12e5e9f306889b5
mbabic <mbabic@ualberta.ca>
2013-11-26 18:18:05 -0700

    code emission boolean flag integrated

--- src/Emit.h
+++ src/Emit.h
@@ -1,8 +1,47 @@
 /*
  * Module -- implements asc code generation facilities.
  */
+#ifndef EMIT_H
+#define EMIT_H
+
+#include <err.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "Error.h"
+#include "Definitions.h"
+#include "StmtLL.h"
+
+#define STMT_LEN 128	/* default size of an asc statement, the only exception
+			 * to this size would be for labels which may have the
+			 * the same name as proc/funcs, in which case we would
+			 * not use this default size */
+extern int doNotEmit;	/* */ 
+#define CHECK_CAN_EMIT(s)\
+do {\
+	if(!(s) || (doNotEmit)) {\
+		doNotEmit = 1;\
+		return;\
+	}\
+} while (0)
+   
+
+extern StmtLL *stmts;
+
+/*
+ * From the given pal filename, this function strips the .pal extension and
+ * appends the .asc extension.
+ * Parameters
+ * 		filename: the name of the file to be renamed
+ */
 char *getAscFileName(char *);
 
 
+void allocStmt(char **, size_t);
+void emitComment(char *);
+void emitVarDecl(Symbol *);
 
 //void emitVarDecl(Symbol *);
+
+#endif
