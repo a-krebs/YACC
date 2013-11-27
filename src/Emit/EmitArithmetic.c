ffeff2a672f35d451bf6ff4817446128312c016a
mbabic <mbabic@ualberta.ca>
2013-11-27 12:17:22 -0700

    emitArithmeticPrep() function factored out from emitAddition -- rest of arithmetic generation code should be very easy

--- src/Emit/EmitArithmetic.c
+++ src/Emit/EmitArithmetic.c
@@ -6,6 +6,62 @@
 #include "EmitArithmetic.h"
 
 /*
+ * Emits asc code to push necessary values of the correct type in order to
+ * perform an arithmetic operation.  Sets the integer pointer result's value
+ * to ARITHMETIC_RESULT_INTEGER or ARITHMETIC_RESULT_REAL such that caller
+ * knows what type of arithmetic operation to perform (real or integer) and
+ * the value on top of th estack.
+ * Parameters:
+ *		x : the LHS operand
+ *		y : the RHS operand
+ *		result : a pointer to an integer
+ */
+void emitArithmeticPrep(Symbol *x, Symbol *y, int *result)
+{
+	CHECK_CAN_EMIT(x);
+	CHECK_CAN_EMIT(y);
+
+	if ((getType(x) == INTEGER_T) && (getType(y) == INTEGER_T)) {
+	
+		/* Emit code to perform arithmetic operation on two integers */
+		emitPushVarValue(x);
+		emitPushVarValue(y);
+		*result = ARITHMETIC_RESULT_INTEGER;
+
+	} else if ((getType(x) == REAL_T) && (getType(y) == REAL_T)) {
+
+		/* Emit code to arithmetic operaton on two reals */
+		emitPushVarValue(x);
+		emitPushVarValue(y);
+		*result = ARITHMETIC_RESULT_INTEGER;
+
+	} else if ((getType(x) == INTEGER_T) && (getType(y) == REAL_T)) {
+		/* 
+		 * Emit code to change x to a real, then emit code to perform
+		 * arithmetic operation on two reals.
+		 */
+		emitPushVarValue(x);
+		emitStmt(STMT_LEN, "ITOR");
+		emitPushVarValue(y);
+		*result = ARITHMETIC_RESULT_REAL;
+	
+	} else if ((getType(x) == REAL_T) && (getType(y) == INTEGER_T)) {
+		/* 
+		 * Emit code to change x to a real, then emit code to perform
+		 * arithmetic operation on two reals.
+		 */
+		emitPushVarValue(y);
+		emitStmt(STMT_LEN, "ITOR");
+		emitPushVarValue(x);
+		*result = ARITHMETIC_RESULT_REAL;
+	}
+
+}
+
+
+
+
+/*
  * Emits asc code to perform an addition operation with the two given symbols
  * and leaves the result on top of the stack
  * Parameters:
@@ -14,6 +70,8 @@
  */
 void emitAddition(Symbol *x, Symbol *y)
 {
+	int result;
+
 	CHECK_CAN_EMIT(x);
 	CHECK_CAN_EMIT(y);
 
@@ -21,39 +79,8 @@ void emitAddition(Symbol *x, Symbol *y)
 	    typeToString(getType(x)), x->name, 
 	    typeToString(getType(y)), y->name);
 
-	if ((getType(x) == INTEGER_T) && (getType(y) == INTEGER_T)) {
-	
-		/* Emit code to add two integers */
-		emitPushVarValue(x);
-		emitPushVarValue(y);
-		emitStmt(STMT_LEN, "ADDI");
+	emitArithmeticPrep(x, y, &result);
+	if (result == ARITHMETIC_RESULT_INTEGER) emitStmt(STMT_LEN, "ADDI");
+	else emitStmt(STMT_LEN, "ADDR");
 
-	} else if ((getType(x) == REAL_T) && (getType(y) == REAL_T)) {
-
-		/* Emit code to add two REALS */
-		emitPushVarValue(x);
-		emitPushVarValue(y);
-		emitStmt(STMT_LEN, "ADDR");
-
-	} else if ((getType(x) == INTEGER_T) && (getType(y) == REAL_T)) {
-		/* 
-		 * Emit code to change x to a real, then emit code to add
-		 * two reals 
-		 */
-		emitPushVarValue(x);
-		emitStmt(STMT_LEN, "ITOR");
-		emitPushVarValue(y);
-		emitStmt(STMT_LEN, "ADDR");
-			
-	} else if ((getType(x) == REAL_T) && (getType(y) == INTEGER_T)) {
-		/* 
-		 * Emit code to change y to a real, then emit code to add
-		 * two reals 
-		 */
-		emitPushVarValue(y);
-		emitStmt(STMT_LEN, "ITOR");
-		emitPushVarValue(x);
-		emitStmt(STMT_LEN, "ADDR");
-			
-	}
 }
