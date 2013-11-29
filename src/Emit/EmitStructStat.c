eb1e53376d005ec749ac1d33dd26a0f4e8f4ce46
Aaron Krebs <akrebs@ualberta.ca>
2013-11-29 15:10:12 -0700

    Cleaned up emitted comments for clarity

--- src/Emit/EmitStructStat.c
+++ src/Emit/EmitStructStat.c
@@ -17,7 +17,8 @@ void emitIfStat(Symbol *s) {
 	/* reserve labels for THEN and ELSE */
 	reserveLabels(labelStack, 2);
 
-	emitComment("IF expr THEN");
+	emitComment("IF expr THEN. If 'expr' is false skip to %s%d",
+	    LABEL_PREFIX, peekLabelStackTop(labelStack));
 	emitStmt(STMT_LEN, "IFZ %s%d", 
 	    LABEL_PREFIX, peekLabelStackTop(labelStack));
 }
@@ -32,7 +33,11 @@ void emitThenMatchedStat() {
 	/* we don't have a symbol pointer, so just pass in non-null */
 	CHECK_CAN_EMIT(1);
 
-	emitComment("THEN as part of IF-THEN-ELSE");
+	emitComment("Bottom of an IF-THEN.");
+	emitComment("Either THEN was executed, so GOTO,");
+	emitComment("or land at %s%d if 'expr' was false",
+	    LABEL_PREFIX, peekLabelStackTop(labelStack));
+	
 	emitStmt(STMT_LEN, "GOTO %s%d",
 	    LABEL_PREFIX, peekLabelStackTop(labelStack) + 1);
 	emitStmt(STMT_LEN, "%s%d:",
@@ -47,8 +52,8 @@ void emitElseStat() {
 	/* we don't have a symbol pointer, so just pass in non-null */
 	CHECK_CAN_EMIT(1);
 
-	emitComment("ELSE stat");
-	emitStmt(STMT_LEN, "%s%d",
+	emitComment("Bottom of ELSE stat");
+	emitStmt(STMT_LEN, "%s%d:",
 	    LABEL_PREFIX, peekLabelStackTop(labelStack) + 1);
 	popLabels(labelStack);
 }
@@ -63,7 +68,7 @@ void emitThenStat() {
 	/* we don't have a symbol pointer, so just pass in non-null */
 	CHECK_CAN_EMIT(1);
 
-	emitComment("THEN as part of IF-THEN");
+	emitComment("Bottom of IF-THEN as part of IF-THEN");
 	emitStmt(STMT_LEN, "%s%d:",
 	    LABEL_PREFIX, peekLabelStackTop(labelStack));
 	popLabels(labelStack);
